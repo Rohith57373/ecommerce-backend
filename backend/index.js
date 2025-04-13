3afd4e69ba514bf19bda53fdf66cec0864885d8b
Rohith R <120489621+573737@users.noreply.github.com>
2025-04-13 19:34:55 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://ecommerce-frontend-alpha-ten.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
